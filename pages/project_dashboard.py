29467e1203be5e42565da98418d17e89c6741901
James Wol <james.wol@petalmail.com>
2026-08-08 03:49:26 +0300

    Update project_dashboard.py

--- pages/project_dashboard.py
+++ pages/project_dashboard.py
@@ -4,7 +4,7 @@ import random
 import uuid
 from main import Building, generate_plan, save_memory, log_event
 from engineering import calculate_total_area, compute_floor_loads, check_structural_integrity, estimate_cost, generate_analysis_report
-from utils import render_svg_plan, generate_3d_html, output_metric, unit_label
+from utils import render_svg_plan_with_grid, generate_3d_html, output_metric, unit_label
 
 def render_project_dashboard():
     st.title("🏢 Project Dashboard")
