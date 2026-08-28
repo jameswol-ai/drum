1c078006fc41b78af9a78cf195ea5315489ca007
James Wol <james.wol@petalmail.com>
2026-08-28 11:00:48 +0300

    Create eurocodes/__init__.py

---
+++ eurocodes/__init__.py
@@ -0,0 +1,11 @@
+# eurocodes/__init__.py
+from .en1990 import *
+from .en1991 import *
+from .en1992 import *
+from .en1993 import *
+from .en1994 import *
+from .en1995 import *
+from .en1996 import *
+from .en1997 import *
+from .en1998 import *
+from .en1999 import *
